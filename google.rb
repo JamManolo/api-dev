423422875e235ce03a591e5e0486b31dcc2536db
James Carrasquer <jimcar@aracnet.com>
2013-10-02 09:39:20 -0700

    Cloud storage test cleanup

--- google.rb
+++ google.rb
@@ -6,14 +6,15 @@ require 'google/api_client'
 require 'json'
 require 'httparty'
 
-# Test storage-object-get and storage-object-insert
-test_object_get = true
-test_object_insert = true
+# Set test flags
+test_bucket_get = true      # storage-bucket-list, storage-bucket_get
+test_object_get = true      # storage-object-list, storage-object-get
+test_object_insert = true   # storage-object-insert
 
-# Update these to match your own apps credentials
-service_account_email = "765280027324-l4aq4o9r9pkk6bjtn8m0dgolkfo0v30j@developer.gserviceaccount.com" # Email of service account
+# Email of service account
+service_account_email = "765280027324-l4aq4o9r9pkk6bjtn8m0dgolkfo0v30j@developer.gserviceaccount.com" 
 key_file = 'privatekey.p12' # File containing your private key
-key_secret = 'notasecret' # Password to unlock private key
+key_secret = 'notasecret'   # Password to unlock private key
 
 client = Google::APIClient.new(application_name: "Soccer App", application_version: "0.1")
 
@@ -32,44 +33,62 @@ client.authorization.fetch_access_token!
 # Discover the Cloud Storage API
 storage = client.discovered_api('storage', 'v1beta2')
 
-# GET https://www.googleapis.com/storage/v1beta2/b?project=free-fantasy-football-info&projection=full&key={YOUR_API_KEY}
-# storage_info = client.execute(:api_method => storage.buckets.list, :parameters => { 
-#   project: "free-fantasy-football-info",
-# })
+# Identify our project space
+project = "free-fantasy-football-info"
 
-# storage_info = client.execute(api_method: storage.buckets.get, parameters: {
-#   bucket: "fff-info-files"
-# })
+# ==============================================
+#  Name: test_bucket_get
+# ==============================================
+if test_bucket_get == true
+
+  bucket_list = client.execute(:api_method => storage.buckets.list, :parameters => { 
+    project: project,
+  })
+
+  buckets = JSON.parse bucket_list.body
+
+  buckets['items'].each do |bucket|
+    bucket_info = client.execute(api_method: storage.buckets.get, parameters: {
+      bucket: bucket['name']
+    })
+  end
+end
+
+# Use bucket from test_get_bucket for object tests, if available
+bucket = buckets.nil? ? "fff-info-files" : buckets['items'].first['name']
 
 # ==============================================
 #  Name: test_object_get
 # ==============================================
+
 if test_object_get == true
 
   # Get the metadata for the objects in our bucket
   object_list = client.execute(:api_method => storage.objects.list, :parameters => { 
-    bucket: "fff-info-files",
+    bucket: bucket,
   })
+
   objects = JSON.parse object_list.body
-  object = objects["items"][2]["name"]
 
-  # Get the metadata for a particular object
-  object_info = client.execute(:api_method => storage.objects.get, :parameters => { 
-    bucket: "fff-info-files",
-    object: object,
-    alt:    "media",
-  })
+  objects['items'].each do |object|
+    # Get the metadata for a particular object
+    object_info = client.execute(:api_method => storage.objects.get, :parameters => { 
+      bucket: bucket,
+      object: object['name'],
+      alt:    "media",
+    })
 
-  # Use HTTParty to download the object - but there's got to be a better way!
-  puts "----- Just HTTParty Hard! -----"
-  url = object_info.response.env[:response_headers]['location']
-  token = "Bearer #{object_info.request.authorization.access_token}"
-  data = HTTParty.get(url, headers: {"Authorization" => token})
-  f = File.open("jmc.objects.get-4.jpg", "w")
-  f.puts data
-  f.close
+    # Use HTTParty to download the object - but there's got to be a better way!
+    puts "----- Just HTTParty Hard! -----"
+    url = object_info.response.env[:response_headers]['location']
+    token = "Bearer #{object_info.request.authorization.access_token}"
+    data = HTTParty.get(url, headers: {"Authorization" => token})
+    f = File.open("./jmc.objects/#{object['name']}", "w")
+    f.puts data
+    f.close
+  end
+
 end
-
 
 # ==============================================
 #  Name: test_object_insert
