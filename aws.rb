4f444ecb5acebffc6a3ce6fd2a87d18d24d6a95f
James Carrasquer <jimcar@aracnet.com>
2013-10-04 12:18:48 -0700

    add fog tests to aws

--- aws.rb
+++ aws.rb
@@ -4,8 +4,8 @@
 require "aws-sdk"
 require "json"
 
-aws_config_keys = JSON.parse(File.open('aws_config.json').read)
-AWS.config(aws_config_keys)
+aws_config = JSON.parse(File.open('aws_config.json').read)
+AWS.config(aws_config)
 
 # Get an instance of the S3 interface
 puts "Getting S3 instance"
@@ -17,7 +17,7 @@ s3.buckets.each do |bucket|
 	bucket.objects.each do |object|
 		next if object.key =~ /^logs\//
 		puts object.key
-		File.open("./jmc.aws.objects/#{object.key}", "wb") do |file|
+		File.open("./downloads/aws/#{object.key}", "wb") do |file|
 			object.read do |chunk|
 				file.write(chunk)
 			end
@@ -36,3 +36,50 @@ upload_files.each do |file_name|
 	s3.buckets[bucket_name].objects[key].write(:file => file_name)
 	puts "Uploaded file #{file_name} to bucket #{bucket_name}."
 end
+
+
+# Now test using Fog
+
+require 'fog'
+
+aws_config = JSON.parse(File.open('aws_config.json').read)
+
+# Get an instance of the Fog/S3 interface
+service = Fog::Storage.new({
+	provider:              'AWS',
+	aws_access_key_id:     aws_config['access_key_id'],
+	aws_secret_access_key: aws_config['secret_access_key'],
+	region:               'us-west-2',
+})
+
+# List the buckets and objects, download all the objects
+puts "------service.directories ------"
+service.directories.each do |dir|
+	puts "KEY = #{dir.key}"
+	dir.files.each do |file|
+		puts "NAME = #{file.key}"
+		next if file.key =~ /^logs\//
+		File.open("./downloads/aws-fog/#{file.key}", 'w') do |f|
+			dir.files.get(file.key) do | data, remaining, content_length |
+				f.syswrite data
+			end
+		end
+	end
+end
+
+# Upload some files
+puts "------- directory.files.create -------"
+if !service.directories.last.nil?
+	dir = service.directories.last
+	puts dir
+	['jmc.log', 'Gemfile'].each do |file|
+		tmpfile = dir.files.create(key: file, body: File.open(file))
+		puts tmpfile
+	end
+end
+
+
+
+
+
+
